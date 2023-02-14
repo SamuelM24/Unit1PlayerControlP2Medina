4680edfecdf20a182a21ca5fdb9acaaeac8f569f
SamuelM24 <beniciomedina11@gmail.com>
2023-02-14 09:53:41 -0800

    finished
    fin

--- PlayerControlMedinaP2/Assets/Scripts/PlayerController.cs
+++ PlayerControlMedinaP2/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float turnSpeed = 45.0f;
-    private float speed = 20.0f;
+    private float turnSpeed = 50.0f;
+    private float speed = 30.0f;
     private float horizontalInput;
     private float forwardInput;
 
@@ -21,7 +21,7 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-        // Move the car forward based on vertical input
+        // Moves the car forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // Rotates the car based on horizontal input
         transform.Rotate(Vector3.up * turnSpeed * horizontalInput * Time.deltaTime);
